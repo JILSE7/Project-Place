82bd4490a73b40be8f9f3edae35f51453ffe4249
Jilse7 <saidnnnn@gmail.com>
2021-04-17 21:13:58 -0500

    f1

--- src/components/search/SearchIdScreen.js
+++ src/components/search/SearchIdScreen.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const SearchIdScreen = () => {
     return (
         <div>
-            <h1>SearchIdScreen</h1>
+            <h1>SearchIdScreen22222</h1>
         </div>
     )
 }
