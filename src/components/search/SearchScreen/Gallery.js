1fd7a2c976ead2b5b0d645bec5925c32afbf8e6e
AntonioMillan <amillan_vlgs96@outlook.com>
2021-04-19 13:38:19 -0500

    added gallery view (pending proofs)

---
+++ src/components/search/SearchScreen/Gallery.js
@@ -0,0 +1,27 @@
+import React from "react";
+import FotoCard from "./FotoCard";
+import PropTypes from "prop-types";
+
+const Gallery = (props) => {
+  return (
+    <div className="container-fluid row align-items-center">
+      {props.fotos.map((foto, index) => {
+        return <FotoCard dataSource={foto} key={index}></FotoCard>;
+      })}
+    </div>
+  );
+};
+
+Gallery.propTypes = {
+  fotos: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      owner: PropTypes.string,
+      imageUrl: PropTypes.string.isRequired,
+      location: PropTypes.string.isRequired,
+      tags: PropTypes.arrayOf(PropTypes.string),
+    })
+  )
+};
+
+export default Gallery;
