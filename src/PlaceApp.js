8e5d52b1e07ce0543476a9b8be4eb5e95f004b83
David Portilla <davidportilla179@hotmail.com>
2021-04-25 21:31:33 -0500

    context

--- src/PlaceApp.js
+++ src/PlaceApp.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { AppRouter } from './routers/AppRouter'
+import { DataProvider } from './context/PlaceContext';
 
 export const PlaceApp = () => {
     return ( 
-        <>
+        <DataProvider>
         <AppRouter/>
-        </>
+        </DataProvider>
     )
 }
