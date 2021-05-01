4b67b4660f54a26632bab68485a473609f65f91b
Said Mandujano <saidnnnn@gmail.com>
2021-04-30 22:29:21 -0500

    relaciones en la base de datos

--- src/helpers/Patch.js
+++ src/helpers/Patch.js
@@ -4,20 +4,6 @@ import {URL} from './Gets'
 
 export const PatchEdit =async(endpoint,id, field, newAtribute )=>{
 
-            try {
-                await fetch(`${URL}/${endpoint}/${id}`, {
-                    method: 'PATCH',
-                    body: JSON.stringify(`{${field}: ${newAtribute}}`),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                    
-                });
-                console.log('dentro del patch')
-                // window.location.href = `http://localhost:3000/search/${id}`;
-            } catch (error) {
-                console.log(error);
-            }
 
    
 }
